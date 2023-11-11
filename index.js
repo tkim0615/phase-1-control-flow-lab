389aa074ebcb0d44b08670d6c8086684708840fe
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-11 13:44:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return "This one is on me!"
+  }else if(feet > 400 && feet < 2000){
+    return "That will be twenty bucks."
+  }else if( feet > 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else {return "No can do."}
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(charm){
   // Write your code here!
-}
+  switch(charm){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
 }
